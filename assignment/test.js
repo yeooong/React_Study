3d3ce7c1f8dfc1f3f8d8e53d223479a9760cf821
yeooong <yeong.kim9127@gmail.com>
2023-02-28 16:10:38 +0900

    to-do-list 하는중

--- assignment/test.js
+++
@@ -1,2 +0,0 @@
-console.log('hi')
-alert('이것은 브라우저에서만')
--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -4,8 +4,39 @@ import './App.css';
 
 function App() {
   return (
-    <div >
-      Hello, World!
+    <div className='layout'>
+
+      <div className='title-square'>
+        <div>My Todo List</div>
+        <div>React</div>
+      </div>
+
+      <form className='input-square'>
+        <div className='input-box'>
+          <label className='input-label'>제목</label>
+          <input type='text' className='input'></input>
+          <label className='input-label'>내용</label>
+          <input type='text' className='input'></input>
+        </div>
+        <button className='input-button'>추가하기</button>
+      </form>
+
+      <div className='list-square'>
+        <h2>Working..❤️‍🔥</h2>
+
+        <div className='list-box'>
+          <div className='todo-box'>
+            <h2 className='todo-title'>리액트 공부하기</h2>
+            <div className='todo-content'>리액트 기초를 공부해봅시다.</div>
+          </div>
+          <div className='list-buttons'>
+            <button className='todo-delete-button'>삭제하기</button>
+            <button className='todo-done-button'>완료</button>
+          </div>
+        </div>
+        <h2>Done..!💖</h2>
+      </div>
+
     </div>
   );
 }
